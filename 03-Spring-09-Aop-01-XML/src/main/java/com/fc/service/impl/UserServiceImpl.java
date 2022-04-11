927d94c80f45dd3db7f17ccdd1abb47ff4e6ee28
wtt <1808791639@qq.com>
2022-04-11 15:36:21 +0800

    MyBaits-00
    第一次提交

---
+++ 03-Spring-09-Aop-01-XML/src/main/java/com/fc/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.fc.service.impl;
+
+import com.fc.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public void add() {
+        System.out.println("添加一个学生");
+    }
+
+    @Override
+    public void update() {
+        int num=1/0;
+        System.out.println("修改了学生信息");
+
+    }
+}
